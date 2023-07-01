97982e1d97bb7676b2a4fd4e2fc4628f5f96235a
cinthiadepaula <cinthiade.paula@outlook.com>
2023-07-01 18:47:48 -0300

    update

--- src/main/java/com/generation/gamestore/controller/ProdutosController.java
+++ src/main/java/com/generation/gamestore/controller/ProdutosController.java
@@ -2,19 +2,27 @@ package com.generation.gamestore.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.gamestore.model.Produtos;
 import com.generation.gamestore.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -35,6 +43,36 @@ public class ProdutosController {
 		return produtosRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Produtos>> getBytitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(produtosRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produtos> post(@Valid @RequestBody Produtos produtos){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(produtosRepository.save(produtos));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produtos> put(@Valid @RequestBody Produtos produtos){
+		return produtosRepository.findById(produtos.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+				.body(produtosRepository.save(produtos)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produtos> produto = produtosRepository.findById(id);
+		
+		if(produto.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		produtosRepository.deleteById(id);
 	}
 
 }
